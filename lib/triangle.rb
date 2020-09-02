c7494f0f3deb144d0dbd287cc2d3d00b020d56f3
Andre King <andreking1031@gmail.com>
2020-09-02 20:47:29 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,6 +2,12 @@ class Triangle
   attr_accessor :equilateral, :isosceles, :scalene
   
   def initialize(equilateral, isosceles, scalene)
+    @equilateral = equilateral
+    @isosceles = isosceles
+    @scalene = scalene
+  end
+  
+  def kind
     
     
   end
